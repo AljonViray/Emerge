e4c65c2ae45f39bb991a9ec0cb7b8b1ed7c87ca0
Shaun Prince <prince.a.shaun@gmail.com>
2019-04-01 02:09:56 -0700

    Initial commit

---
+++ Emerge/Assets/_Scripts/PlayerCameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraControl : MonoBehaviour
+{
+    public float lookSensitivity;
+
+    private float _deltaX;
+    private float _deltaY;
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        _camera = this.GetComponentInChildren<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+    }
+
+    private void Look()
+    {
+        _deltaX = Input.GetAxis("Mouse X");
+        _deltaY = Input.GetAxis("Mouse Y");
+        _camera.transform.Rotate(-_deltaY * lookSensitivity, 0f, 0f);
+        this.transform.Rotate(0f, _deltaX * lookSensitivity, 0f);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ Emerge/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float baseMovespeed;
+    public float baseJumpForce;
+
+    private Rigidbody _rb;
+
+    private float _deltaX;
+    private float _deltaY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _deltaX = Input.GetAxisRaw("Horizontal");
+        _deltaY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        _rb.AddRelativeForce(_deltaX * baseMovespeed, 0f, _deltaY * baseMovespeed, ForceMode.VelocityChange);
+    }
+
+
+}
