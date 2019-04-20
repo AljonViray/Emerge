7a819aa77698ca9e4a08dac09dc9bd4c8eb42e47
Aljon Viray <asviray@uci.edu>
2019-04-20 16:06:48 -0700

    Darts stick to dartboard & can distinguish between rings

--- Emerge/Assets/_Scripts/Dart.cs
+++ Emerge/Assets/_Scripts/Dart.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Dart : CarryableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public override bool Interact(PlayerInteraction interactor)
     {
         Debug.Log("Interact via DART");
---
+++ Emerge/Assets/_Scripts/Dartboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dartboard : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Interactable" && collision.gameObject.name.Split('_')[0] == "Dart")
+        {
+            Debug.Log("Dart stuck to " + this.gameObject.name);
+            collision.rigidbody.isKinematic = true;
+        }
+    }
+}
