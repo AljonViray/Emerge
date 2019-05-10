8f1baf908bcd6b3fb37247a0922cfd07a4097da9
Shaun <prince.a.shaun@gmail.com>
2019-05-10 10:43:24 -0700

    Added timer system to Game Manager

--- Emerge/Assets/_Scripts/GameManager.cs
+++ Emerge/Assets/_Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public string[] hints;
     public PuzzleParent[] puzzleParents;
     public int currentPuzzle;
+    public float timeRemaining;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeRemaining -= Time.deltaTime;
+        if(timeRemaining <= 0)
+        {
+            timeRemaining = 0;
+            Debug.Log("GAME OVER");
+        }
+        this.GetComponentInChildren<TextMeshProUGUI>().text = (timeRemaining/60f).ToString("0.00");
     }
 
     private void SetAllPuzzlesToUnsolved()
