b1b44dd773a6ecae933661119a20c730b09f2a75
Aljon Viray <asviray@uci.edu>
2019-05-17 16:36:22 -0700

    Barrel Minigame Fix & Note Spawns after winning it

--- Emerge/Assets/_Scripts/Barrel Scripts/BarrelRack.cs
+++ Emerge/Assets/_Scripts/Barrel Scripts/BarrelRack.cs
@@ -7,6 +7,7 @@ public class BarrelRack : MonoBehaviour
     public List<string> attempt = new List<string>();
     public List<string> solution;
     public GameObject resetButton;
+    public GameObject noteFragment;
     public bool isSolved = false;
 
     private GameObject player;
@@ -24,7 +25,7 @@ public class BarrelRack : MonoBehaviour
         resetButton = GameObject.Find("Reset_Barrels");
 
         solution = new List<string> { "Position_1 (1)", "Position_1 (3)", "Position_2 (3)",
-                                      "Position_2 (1)", "Position_1 (2)", "Position_2 (2)" };
+                                      "Position_2 (1)", "Position_1 (2)" };
 
         barrels = new List<GameObject> { GameObject.Find("Barrel_1"), GameObject.Find("Barrel_2"), GameObject.Find("Barrel_3"),
                                          GameObject.Find("Barrel_4"), GameObject.Find("Barrel_5"), GameObject.Find("Barrel_6") };
@@ -33,7 +34,7 @@ public class BarrelRack : MonoBehaviour
 
     void Update()
     {
-        if (attempt.Count >= 6 && isSolved == false) CheckSolution();
+        if (attempt.Count >= 5 && isSolved == false) CheckSolution();
         if (player.GetComponent<PlayerInteraction>().lookingAt() == resetButton && Input.GetKeyDown(KeyCode.E)) Reset();
     }
 
@@ -65,20 +66,26 @@ public class BarrelRack : MonoBehaviour
         Debug.Log("Checking Solution of Barrels...");
         // Checking is it fails
         for (int i = 0; i < solution.Count; i++)
-            if (attempt[i] != solution[i]) return;
+            if (!solution.Contains(attempt[i])) return;
 
-        // If successful, play animation and freeze dartboard/darts
+        // If successful, play animation and freeze barrels (if it's on the rack)
         for (int i = 0; i < barrels.Count; i++)
         {
-            barrels[i].transform.SetParent(this.gameObject.transform);
-            barrels[i].tag = "Untagged";
+            if (barrels[i].GetComponent<Rigidbody>().isKinematic)
+            {
+                barrels[i].transform.SetParent(this.gameObject.transform);
+                barrels[i].tag = "Untagged";
+            }
         }
  
         Debug.Log("Barrel Minigame COMPLETE!");
         isSolved = true;
         GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().PuzzleSolved();
-        //transform.parent.GetComponent<Animator>().SetTrigger("isComplete");
-        
+
+        // "Spawn" the Note Fragment after winning
+        noteFragment.transform.GetChild(0).gameObject.SetActive(true);
+        noteFragment.GetComponent<Rigidbody>().isKinematic = false;
+
         // Prevents script from running anymore
         Destroy(resetButton);
         this.gameObject.GetComponent<BarrelRack>().enabled = false;
