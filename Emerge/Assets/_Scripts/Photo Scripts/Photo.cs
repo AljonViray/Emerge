e6e0cedeb0a6904f6900df4e125d6f213497cceb
Aljon Viray <asviray@uci.edu>
2019-05-16 19:00:25 -0700

    Simple Version of Photo Minigame Done
    - Plus some more Unity Store assets/materials

---
+++ Emerge/Assets/_Scripts/Photo Scripts/Photo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photo : MonoBehaviour
+{
+    public GameObject player;
+    public List<string> solution;
+    public GameObject lookingAt;
+    public bool isSolved = false;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        solution = new List<string> { "Position_1", "Position_2", "Position_3",
+                                      "Position_4", "Position_5" };
+    }
+
+    void Update()
+    {
+        lookingAt = player.GetComponent<PlayerInteraction>().lookingAt();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (solution.Contains(lookingAt.name))
+            {
+                lookingAt.GetComponent<MeshRenderer>().enabled = true;
+                solution.Remove(lookingAt.name);
+            }
+
+            if (solution.Count == 0)
+            {
+                isSolved = true;
+                this.gameObject.GetComponent<Photo>().enabled = false;
+            }
+        }
+    }
+}
