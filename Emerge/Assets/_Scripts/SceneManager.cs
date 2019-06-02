fd54687d647ee49b84061ec029d149dc17d35b97
gavindent <gavindent@hotmail.com>
2019-06-01 20:08:31 -0700

    Main Menu and Game Over scenes added. Scene Manager prefab created. Buttons will load scenes once they are put into the build.

---
+++ Emerge/Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public string mainGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMainGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(mainGame);
+    }
+
+    public void LoadGameOver()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game Over");
+    }
+
+    public void LoadMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Application.Quit();
+    }
+}
