38226de71c5c5f0d6cab0aac2a40471e881888b4
gavindent <gavindent@hotmail.com>
2019-06-03 21:20:32 -0700

    Added credits to Main Menu with TextMeshPro.

---
+++ Emerge/Assets/_Scripts/CreditsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour
+{
+    private GameObject[] myMenuUI;
+    private GameObject myCredits;
+    private GameObject myBackButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMenuUI = new GameObject[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            myMenuUI[i] = this.transform.GetChild(i+1).gameObject;
+        }
+        myCredits = this.transform.GetChild(5).gameObject;
+        myBackButton = this.transform.GetChild(6).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ViewCredits()
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            myMenuUI[i].SetActive(false);
+        }
+        myCredits.SetActive(true);
+        myBackButton.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            myMenuUI[i].SetActive(true);
+        }
+        myCredits.SetActive(false);
+        myBackButton.SetActive(false);
+    }
+}
