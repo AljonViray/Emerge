d021f4f918881fc5c3b968458c645b400aff1ae5
Shaun Prince <prince.a.shaun@gmail.com>
2019-06-03 23:30:35 -0700

    fixed some level 1 bugs

--- Emerge/Assets/_Scripts/NoteBoard.cs
+++ Emerge/Assets/_Scripts/NoteBoard.cs
@@ -19,6 +19,7 @@ public class NoteBoard : MonoBehaviour
                 = anchors[other.gameObject.GetComponentInParent<NoteFragment>().orderInReceipe].transform;
             other.gameObject.transform.parent.transform.localPosition = Vector3.zero;
             other.gameObject.transform.parent.transform.localRotation = Quaternion.Euler(new Vector3(0,90,0));
+            Destroy(other.gameObject.transform.parent.gameObject.GetComponent<NoteFragment>());
             other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody>().isKinematic = true;
         }
     }
